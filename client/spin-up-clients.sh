ee70fdcf0616fa00d897174178564f055fbb6777
lakindu hewawasam <lakindu2002@live.com>
2023-04-18 17:57:36 +0530

    removing script

--- client/spin-up-clients.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PORTS=(3000 3001 3002)
-PROJECT_ROOT=<<PATH-TO-ROOT>>
-
-for ((i=0; i<${#PORTS[@]}; i++)); do
-  osascript -e "tell application \"Terminal\" to do script \"cd $PROJECT_ROOT && npm start -- --port=${PORTS[$i]} --host=node:${PORTS[$i]}\""
-done
